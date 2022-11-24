2c23338461e909c3084416988e7262ab65800435
rathinkarmakar <rathin.karmakar@nagarro.com>
2022-11-24 15:48:21 +0530

    BookModel and Repository added

---
+++ LiveApp/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using LiveApp.Models;
+using LiveApp.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public BookRepository _bookRepository { get; set; }
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBook(string authorName)
+        {
+            return _bookRepository.SearchBook(authorName);
+        }
+    }
+}
---
+++ LiveApp/Repository/BookRepository.cs
@@ -0,0 +1,42 @@
+﻿using LiveApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveApp.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string author)
+        {
+            return DataSource().Where(x => x.Author.Contains(author)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>
+            {
+                new BookModel() { Id = 1, Author = "Rathin Karmakar", Name = "How to live a life" },
+                new BookModel() { Id = 2, Author = "Tapendu Karmakar", Name = "Sea of life" },
+                new BookModel() { Id = 3, Author = "Rathin Das", Name = "How to draw" },
+                new BookModel() { Id = 4, Author = "Subendu Mondal", Name = "What is life" },
+                new BookModel() { Id = 5, Author = "Smriti Das", Name = "Life is short" },
+                new BookModel() { Id = 6, Author = "Suman Karmakar", Name = "Live yourself" },
+                new BookModel() { Id = 7, Author = "Rathin Bhattacharya", Name = "Dont give up" },
+                new BookModel() { Id = 8, Author = "Ronty Pal", Name = "Life is a journey" },
+                new BookModel() { Id = 9, Author = "Ankit Pal", Name = "Travel the world" }
+            };
+        }
+    }
+}
