7ceafaeb732d62009ad2f71ab95c435d6791a615
rathinkarmakar <rathin.karmakar@nagarro.com>
2022-11-24 11:26:43 +0530

    Controller added

---
+++ LiveApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveApp.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello from Rathin";
+        }
+    }
+}
