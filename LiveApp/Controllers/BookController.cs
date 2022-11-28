fed8281a3923ae7364527d1f3bbc901a272b6a35
rathinkarmakar <rathin.karmakar@nagarro.com>
2022-11-28 08:52:51 +0530

    Partial and Dynamic Partial views are added

--- LiveApp/Controllers/BookController.cs
+++ LiveApp/Controllers/BookController.cs
@@ -10,6 +10,8 @@ namespace LiveApp.Controllers
 {
     public class BookController : Controller
     {
+        [ViewData]
+        public string Title { get; set; }
         public BookRepository _bookRepository { get; set; }
         public BookController()
         {
@@ -18,18 +20,32 @@ namespace LiveApp.Controllers
 
         public ViewResult GetAllBooks()
         {
+            Title = "All Books";
             var list = _bookRepository.GetAllBooks();
             return View(list);
         }
 
         public ViewResult GetBookById(int id)
         {
+            Title =_bookRepository.GetBookById(id).Name + " Book Details";
+            ViewBag.Book = _bookRepository.GetBookById(id);
             return View(_bookRepository.GetBookById(id));
         }
 
         public ViewResult SearchBook(string authorName)
         {
             //var list = _bookRepository.SearchBook(authorName);
+            return View();
+        }
+
+        public ViewResult AddNewBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult AddNewBook(BookModel bookModel)
+        {
             return View();
         }
     }
--- LiveApp/Controllers/HomeController.cs
+++ LiveApp/Controllers/HomeController.cs
@@ -9,18 +9,23 @@ namespace LiveApp.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string Title { get; set; }
         public ViewResult Index()
         {
+            Title = "Home";
             return View();
         }
 
         public ViewResult AboutUs()
         {
+            Title = "About Us";
             return View();
         }
 
         public ViewResult ContactUs()
         {
+            Title = "Contact Us";
             return View();
         }
     }
